aefa12358707399829cf4e79d6d79fd4aed93097
mlengse <anjangkn@gmail.com>
2026-08-03 01:06:41 +0700

    remove legacy orthos_colab implementation and configuration files

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="orthos",
-    version="0.1.0",
-    py_modules=["orthos_colab"],
-    python_requires=">=3.10",
-    install_requires=["numpy", "numba"],
-)
